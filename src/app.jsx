436c92e5a2cba2584da3770f0a79144b254a7955
Vibhav Churi <vibhav.churi@cimpress.com>
2018-07-31 10:49:12 +0530

    Added form to app

--- src/app.jsx
+++ src/app.jsx
@@ -6,19 +6,30 @@ const app = {
 };
 
 function getOptions(options) {
-  let jsx;
-  for (const option of options) {
-    jsx += <li>{option}</li>;
-  }
-  return jsx;
+  return <p>{options.length}</p>;
 }
 
-const template = (
-  <div>
-    <h3>{app.title}</h3>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options && app.options.length > 0 ? "Here are your options" : "No Options"}</p>
-    {getOptions(app.options)}
-  </div>
-);
-ReactDOM.render(template, document.getElementById("app"));
+function onFormSubmit(e) {
+  e.preventDefault();
+  const option = e.target.option.value;
+  app.options.push(option);
+  render();
+}
+
+function render() {
+  const template = (
+    <div>
+      <h3>{app.title}</h3>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options && app.options.length > 0 ? "Here are your options" : "No Options"}</p>
+      {getOptions(app.options)}
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+  ReactDOM.render(template, document.getElementById("app"));
+}
+
+render();
